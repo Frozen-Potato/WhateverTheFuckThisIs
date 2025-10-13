ceb7a79ccdad6d141012f474d9e54ee4c405c6a9
Kien Pham <mcfkcoding@gmail.com>
2025-10-13 10:22:24 +0700

    Add files via upload

---
+++ Main-Example/Library/includes/PostgresAdapter.h
@@ -0,0 +1,46 @@
+#ifndef POSTGRES_ADAPTER_H
+#define POSTGRES_ADAPTER_H
+
+#include <pqxx/pqxx>
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+
+#include "Member.h"
+#include "Media.h"
+#include "Book.h"
+#include "Magezine.h"
+#include "BorrowRecord.h"
+#include "EnvLoader.h"
+#include "Student.h"
+#include "Teacher.h"
+
+class PostgresAdapter {
+private:
+    pqxx::connection conn;
+    std::string getConnectionFromEnv();
+
+public:
+    PostgresAdapter();
+    ~PostgresAdapter();
+
+    // --- Member operations ---
+    void addMember(const Member& member);
+    std::vector<Member> getAllMembers();
+
+    // --- Media operations ---
+    std::shared_ptr<Media> getMediaById(const int& id);
+    void addBook(const Book& book);
+    void addMagazine(const Magazine& mag);
+    std::vector<std::shared_ptr<Media>> getAllMedia();
+    void updateMediaAvailability(int mediaId, bool available);
+
+    // --- Borrow operations ---
+    void addBorrowRecord(int userId, int mediaId);
+    void markReturned(int borrowId);
+    std::vector<BorrowRecord> getAllBorrowRecords();
+};
+
+#endif
