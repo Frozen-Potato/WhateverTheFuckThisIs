1eaaa2d446b95246ff16c4896abf0aef75d0ec4e
Kien Pham <mcfkcoding@gmail.com>
2025-10-13 10:19:50 +0700

    Add files via upload

--- Main-Example/Library/src/server/RestServer.cpp
+++ Main-Example/Library/src/server/RestServer.cpp
@@ -15,7 +15,7 @@ void openRestServer() {
     Library library;
 
     // GET /medias
-    svr.Get("/medias", [&](const httplib::Request&, httplib::Response& res) {
+    svr.Get("/media", [&](const httplib::Request&, httplib::Response& res) {
         auto medias = library.getAllMedias();
         nlohmann::json j = nlohmann::json::array();
 
@@ -42,6 +42,77 @@ void openRestServer() {
         }
         res.set_content(j.dump(), "application/json");
     });
+
+    svr.Post("/media", [&](const httplib::Request&, httplib::Response& res){
+        try {
+            auto body = nlohmann::json::parse(req.body);
+
+            if (!j.contains("media_type") || !j.contains("id")) {
+                res.status = 400;
+                res.set_content(R"({"error": "Missing 'media_type' or 'id' field"})", "application/json");
+                return;
+            }
+
+            if (library.findItemById(j['id']) != nullptr){
+                res.status = 400;
+                res.set_content(R"({"error": "Duplicate id"})", "application/json");
+                return;
+            };
+
+            std::string mediaType = j["media_type"];
+
+            if (mediaType == "BOOK") {
+                if (!j.contains("title") || !j.contains("author")) {
+                    res.status = 400;
+                    res.set_content(R"({"error": "Books require 'title' and 'author'"})", "application/json");
+                    return;
+                }
+                if (!j["title"].is_string() || !j["author"].is_string()) {
+                    res.status = 400;
+                    res.set_content(R"({"error": "Invalid field types"})", "application/json");
+                    return;
+                }
+                
+                library.addBook(j["id"], j["title"], j["author"]);
+                
+                res.status = 201;
+                res.set_content(R"({"SUCCESS": "Item added successfully"})", "application/json");
+                
+            } else if (mediaType == "MAGAZINE") {
+                if (!j.contains("title") || !j.contains("issue_number")) {
+                    res.status = 400;
+                    res.set_content(R"({"error": "Magazines require 'title' and 'issue_number'"})", "application/json");
+                    return;
+                }
+                if (!j["title"].is_string() || !j["issue_number"].is_number_integer()) {
+                    res.status = 400;
+                    res.set_content(R"({"error": "Invalid field types"})", "application/json");
+                    return;
+                }
+                
+                library.addMagazine(j["id"], j["title"], j["issue_number"]);
+                
+                res.status = 201;
+                res.set_content(R"({"SUCCESS": "Item added successfully"})", "application/json");
+                
+            } else {
+                res.status = 400;
+                nlohmann::json error = {
+                    {"error", "Invalid media_type"},
+                    {"allowed_values", {"BOOK", "MAGAZINE"}}
+                };
+                res.set_content(error.dump(), "application/json");
+            }
+            
+        } catch (nlohmann::json::parse_error& e) {
+            res.status = 400;
+            res.set_content(R"({"error": "Invalid JSON"})", "application/json");
+        } catch (std::exception& e) {
+            res.status = 500;
+            res.set_content(R"({"error": "Internal server error"})", "application/json");
+        }
+
+})
 
     // POST /borrow
     svr.Post("/borrow", [&](const httplib::Request& req, httplib::Response& res) {
